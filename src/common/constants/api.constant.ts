e10f664c6d87934740fb6778883df0abee1cdde0
htndev <htn.developer@gmail.com>
2022-11-24 00:01:49 +0100

    chore: config update

--- src/common/constants/api.constant.ts
+++ src/common/constants/api.constant.ts
@@ -1,7 +1,9 @@
 export const BASE_API_URL = process.env.REACT_APP_API_URL;
 
+export const BASE_API_ROUTES_URL = process.env.REACT_APP_API_ROUTES_URL;
+
 export const API_VERSION = process.env.REACT_APP_API_VERSION;
 
 export const BASE_URL = `${BASE_API_URL}/${API_VERSION}`;
 
-export const API_ROUTES = `${BASE_URL}/routes`;
+export const API_ROUTES = `${BASE_API_ROUTES_URL}/routes`;
